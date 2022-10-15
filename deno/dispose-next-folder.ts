349649a77def5fe20d1f24b556fb27ec0c6c347b
wisdomstar94 <wisdomstar94@gmail.com>
2022-10-15 17:37:21 +0900

    try catch 추가.

--- deno/dispose-next-folder.ts
+++ deno/dispose-next-folder.ts
@@ -3,15 +3,30 @@ import * as path from "https://deno.land/std@0.159.0/path/mod.ts";
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
 // .next.old 폴더 삭제
-await Deno.remove(path.join(__dirname, '..', '.next.old'), { recursive: true });
+try {
+  await Deno.remove(path.join(__dirname, '..', '.next.old'), { recursive: true });
+} catch (e) {
+
+}
 
 // .next 폴더 이름을 .next.old 으로 변경
-await move(path.join(__dirname, '..', '.next/'), path.join(__dirname, '..', '.next.old/'));
+try {
+  await move(path.join(__dirname, '..', '.next/'), path.join(__dirname, '..', '.next.old/'));
+} catch (e) {
+
+}
 
 // .next.new/.next 폴더를 .next 으로 이동
-await move(path.join(__dirname, '..', '.next.new', '.next/'), path.join(__dirname, '..', '.next/'));
+try {
+  await move(path.join(__dirname, '..', '.next.new', '.next/'), path.join(__dirname, '..', '.next/'));
+} catch (e) {
+
+}
 
 // .next.new 폴더 삭제
-await Deno.remove(path.join(__dirname, '..', '.next.new'), { recursive: true });
-
+try {
+  await Deno.remove(path.join(__dirname, '..', '.next.new'), { recursive: true });
+} catch (e) {
+  
+}
 // deno run --allow-write --allow-read ${path}
