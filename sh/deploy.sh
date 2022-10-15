a063e884f437169b1d99ba00a289a763b3328f09
wisdomstar94 <wisdomstar94@gmail.com>
2022-10-15 17:42:26 +0900

    deno 절대경로로 변경.

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,5 @@
 pushd /home/projects/gofield-frontend
-deno run --allow-write --allow-read ./deno/dispose-next-folder.ts
+~/.deno/bin/deno run --allow-write --allow-read ./deno/dispose-next-folder.ts
 git pull origin main
 npm i
 pm2 reload "gofield-frontend"
