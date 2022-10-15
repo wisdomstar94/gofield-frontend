138e929a8cf6f7feaf3c657ee989a7632d1386fd
wisdomstar94 <wisdomstar94@gmail.com>
2022-10-15 17:49:40 +0900

    reload 에서 restart 로 변경.

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -2,5 +2,5 @@ pushd /home/projects/gofield-frontend
 ~/.deno/bin/deno run --allow-write --allow-read ./deno/dispose-next-folder.ts
 git pull origin main
 npm i
-pm2 reload "gofield-frontend"
+pm2 restart "gofield-frontend"
 popd
