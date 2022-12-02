94f5fe857bfed6cf7d44e3e4e8548971962ada6c
wisdomstar94 <wisdomstar94@gmail.com>
2022-12-02 19:24:10 +0900

    order-complete 와 order-fail 페이지만 우선 생성.

---
+++ src/pages/order-fail/index.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import AccessTokenCheck from "../../components/auth/access-token-check/access-token-check.component";
+import Topbar from "../../components/layouts/top-bar/top-bar.component";
+import WindowSizeContainer from "../../components/layouts/window-size-container/window-size-container.component";
+
+const ProductNewPage = () => {
+  return (
+    <>
+      <Head>
+        <title>주문 실패</title>
+        <meta name="description" content="고필드 주문 실패 페이지입니다." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AccessTokenCheck __checkTarget="signup-complete-user">
+        <PageContents />
+      </AccessTokenCheck>
+    </>
+  );
+};
+
+const PageContents = () => {
+  return (
+    <>
+      <WindowSizeContainer __bgColor="#fff">
+        <Topbar
+          __layoutTypeB={{
+            titleComponent: <>주문 실패</>,
+            rightComponent: <></>,
+          }} />
+        ...
+      </WindowSizeContainer>
+    </>
+  );
+};
+
+export default ProductNewPage;
