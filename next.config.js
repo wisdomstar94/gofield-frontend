0f0b27b4ae749022626a65e765853798b3b54d11
wisdomstar94 <wisdomstar94@gmail.com>
2022-12-27 13:32:59 +0900

    next.js scss 전역 변수 환경 셋팅

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -8,6 +10,10 @@ const nextConfig = {
       "gofield.s3.ap-northeast-2.amazonaws.com",
       "d22b51gh5o1q1e.cloudfront.net",
     ],
+  },
+  sassOptions: {
+    // includePaths: [path.join(__dirname, 'src/styles/common')],
+    prependData: `@import "src/styles/common/common.scss";`, // common.scss 에 선언한 scss 변수를 모든 scss 파일에서 사용 가능!
   },
 };
 
