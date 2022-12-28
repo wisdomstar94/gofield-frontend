55abafa5873bc1f85a3a403dcff733319d786277
wisdomstar94 <wisdomstar94@gmail.com>
2022-12-28 10:04:32 +0900

    메인페이지의 슬라이딩 배너 부분을 Swiper 라이브러리로 교체.

--- src/components/boxes/banner-box/banner-box.component.tsx
+++ src/components/boxes/banner-box/banner-box.component.tsx
@@ -5,6 +5,14 @@ import Image from 'next/image';
 import useMainBannerListQuery from "../../../hooks/use-queries/use-main-banner-list.query";
 import { useCallback, useRef } from "react";
 import { IBanner } from "../../../interfaces/banner/banner.interface";
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import { getClasses } from "../../../librarys/string-util/string-util.library";
 
 const BannerBox = (props: IBannerBox.Props) => {
   const mainBannerListQuery = useMainBannerListQuery();
@@ -22,10 +30,39 @@ const BannerBox = (props: IBannerBox.Props) => {
 
   return (
     <>
-      <div className={[
+      <div className={getClasses([
           styles['banner-box'],
-        ].join(' ')}>
-        <SwiperCustom 
+        ])}>
+        <Swiper
+          navigation
+          pagination={{ clickable: true }}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+          autoplay={{ delay: 3000 }}
+        >
+          {
+            mainBannerListQuery?.data?.map((item, index) => {
+              return (
+                <SwiperSlide 
+                  key={index}>
+                  <div className={styles['banner-item']}>
+                    <Image
+                      priority
+                      src={item.thumbnail}
+                      alt={item.description}
+                      title={item.title}
+                      layout="fill"
+                      draggable={false}
+                      objectFit="cover" />
+                  </div>
+                </SwiperSlide>
+              );
+            })
+          }
+        </Swiper>
+
+        {/* <SwiperCustom 
           __style={{ aspectRatio: '16 / 9' }} 
           __onChange={swiperChange} 
           __onItemClick={onItemClick}
@@ -49,7 +86,7 @@ const BannerBox = (props: IBannerBox.Props) => {
               );
             })
           }
-        </SwiperCustom>
+        </SwiperCustom> */}
       </div>
     </>
   );
